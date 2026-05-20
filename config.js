775c5283f6812a4767a01c51367cb3404c4c63dd
OneMoreFix <onemorefix1337@gmail.com>
2026-05-20 14:53:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // замени на свой cloudflare tunnel url
 
 window.SENTINEL_MUSIC_CONFIG = {
-  API_URL: "https://alex-beings-transportation-previous.trycloudflare.com",
+  API_URL: "https://walt-occupational-libs-survival.trycloudflare.com",
 };
