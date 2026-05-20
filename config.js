4f75f1b32934af9e3b36dc01761ac358dc9aa1b1
OneMoreFix <onemorefix1337@gmail.com>
2026-05-20 07:05:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // замени на свой cloudflare tunnel url
 
 window.SENTINEL_MUSIC_CONFIG = {
-  API_URL: "https://marion-textiles-accepting-alias.trycloudflare.com",
+  API_URL: "https://doe-farmer-manga-von.trycloudflare.com",
 };
